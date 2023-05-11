c505127a16c06ba2e159d3b59098000333c5a49f
GustavoPSA <gustavosarria999@gmail.com>
2023-05-11 14:38:45 -0500

    Base inicial del Proyecto Back

---
+++ src/main/java/com/servicios_publicos/back/app/ServiciosPublicosBackApplication.java
@@ -0,0 +1,13 @@
+package com.servicios_publicos.back.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosPublicosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosPublicosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicios_publicos/springboot/back/app/ServiciosPublicosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicios_publicos.springboot.back.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosPublicosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
